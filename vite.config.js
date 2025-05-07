f53271df9cf798c14c9d930ed1f918871e51aea5
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-05-07 13:37:17 +0800

    update

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+    server: {
+      host: '0.0.0.0',  // Bind to 0.0.0.0 to allow external access
+      port: 4000,        // O kung anong port ang gusto mong gamitin
+    }
+  }
+  
