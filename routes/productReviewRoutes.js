468289859c943b119af3e28c3668b649caf5159b
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-05-13 14:45:41 +0800

    update

--- routes/productReviewRoutes.js
+++ routes/productReviewRoutes.js
@@ -9,9 +9,9 @@ import authUser from '../middleware/admin.js';
 
 const router = express.Router();
 
-router.post('/add', userAuth, addReview);
+router.post('/add', authUser, addReview);
 router.get('/:productId', getReviewsByProduct);
-router.delete('/:reviewId', userAuth, deleteReview);
+router.delete('/:reviewId', authUser, deleteReview);
 router.get('/can-review/:productId/:userId', authUser, canReviewProduct);
 
 export default router;
