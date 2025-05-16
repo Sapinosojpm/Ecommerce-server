c0914363b83444d307fa0687aab41178fa149090
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-05-16 11:50:55 +0800

    update

--- models/returnModel.js
+++ models/returnModel.js
@@ -8,7 +8,7 @@ const returnSchema = new mongoose.Schema({
   },
   userId: { 
     type: mongoose.Schema.Types.ObjectId, 
-    ref: "user", 
+    ref: "User", 
     required: true 
   },
   itemId: { 
