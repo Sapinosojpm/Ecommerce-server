1f64ddce8ba1c1a6354f2a9a8db2d70d761c8bd3
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-06-05 14:26:40 +0800

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const authUser = async (req, res, next) => {
     let token = req.headers.token || req.headers['authorization'];
 
     if (token && token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length); // Remove "Bearer " prefix
+        token = token.slice(7, token.length);
     }
 
     if (!token) {
@@ -13,13 +13,14 @@ const authUser = async (req, res, next) => {
 
     try {
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-        req.userEmail = token_decode.email; // Extract user email from token
-        req.body.userId = token_decode.id;  // Extract user ID from token
+        req.userEmail = token_decode.email;
+        req.userId = token_decode.id;  // Fix here
         next();
     } catch (error) {
         console.error('Token verification error:', error);
         return res.status(401).json({ success: false, message: 'Invalid or expired token. Please log in again.' });
     }
 };
+
 
 export default authUser;
