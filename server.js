5c578c2a36109046989441130650f7b7d7c25e8c
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-06-16 17:01:22 +0800

    update

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ if (!fs.existsSync(uploadPath)) {
 app.use(express.json());
 const allowedOrigins = [
   process.env.FRONTEND_URL || "http://localhost:5173",
-  "http://localhost:5174",
+  "http://localhost:5174",'https://ecommerce-frontend-peach-rho.vercel.app/','https://ecommerce-frontend-admin-cyan.vercel.app/'
 ];
 
 app.use(cors({
