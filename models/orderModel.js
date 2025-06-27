f00f665a663bb32ab47ff9c15f7b69e43363ddc5
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-06-27 15:55:49 +0800

    update

--- models/orderModel.js
+++ models/orderModel.js
@@ -87,6 +87,7 @@ const orderSchema = new mongoose.Schema(
       type: String,
       enum: [
         "Order Placed",
+        "Packing",
         "Pending",
         "Processing",
         "Shipped",
