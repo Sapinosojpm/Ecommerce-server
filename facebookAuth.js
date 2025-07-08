650d945c53e7c4b029028b1e0719bb69e0dcbd60
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-07-08 13:09:33 +0800

    update

--- facebookAuth.js
+++ facebookAuth.js
@@ -6,6 +6,7 @@ dotenv.config();
 passport.serializeUser((user, done) => {
   done(null, user);
 });
+
 passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
@@ -13,16 +14,20 @@ passport.deserializeUser((obj, done) => {
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL || `${process.env.BACKEND_URL}/api/auth/facebook/callback`,
     profileFields: ['id', 'displayName', 'emails'],
     enableProof: true,
-    scope: ['email', 'pages_show_list', 'pages_read_engagement', 'pages_manage_posts']
+    scope: ['email', 'pages_show_list', 'pages_read_engagement', 'pages_manage_posts'],
+    state: true
   },
   function(accessToken, refreshToken, profile, done) {
-    // You can store accessToken in DB for the user here
-    profile.accessToken = accessToken;
-    return done(null, profile);
+    // Store the accessToken with the profile
+    const user = {
+      ...profile,
+      accessToken: accessToken
+    };
+    return done(null, user);
   }
 ));
 
-export default passport; 
+export default passport;
