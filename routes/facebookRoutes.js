ded4be1966758a553b16f9f34e8b57f01089d39d
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-07-08 16:45:08 +0800

    update

--- routes/facebookRoutes.js
+++ routes/facebookRoutes.js
@@ -12,11 +12,11 @@ const router = express.Router();
 // Facebook OAuth start
 router.get('/auth/facebook', facebookAuth);
 // Facebook OAuth callback
-router.get('/auth/facebook/callback', ...facebookCallback);
+router.get('/auth/facebook/callback', facebookCallback);
 // Facebook login error
 router.get('/login', facebookLoginError);
 // Facebook API endpoints
-router.get('/facebook/pages', ...getPages);
-router.post('/facebook/post', ...postToPage);
+router.get('/facebook/pages', getPages);
+router.post('/facebook/post', postToPage);
 
 export default router;
